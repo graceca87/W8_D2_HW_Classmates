22f4264d471ee85c34c31e6590daf79ba378890a
Grace Alfonsi <cunniff.grace@gmail.com>
2022-08-30 21:56:50 -0500

    added Classmates & Navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ export default function App() {
     return (
        <>
        <Navbar />
-       <div class='container'>
-        <Classmates />
+       <div className='container'>
+            <Classmates />
        </div>
        </>
   )
--- src/components/Classmates.jsx
+++ src/components/Classmates.jsx
@@ -1,9 +1,51 @@
 // function to fetch the API data
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function classmates() {
+export default function Classmates() {
+    const cardStyle = {
+        width: '18rem'
+    }
+
+    const buttonStyle = {
+        backgroundColor: '#6a4c93'
+    }
     
+    // setClassmates is a function that takes in classmates data
+    const [classmates, setClassmates] = useState([])
+
+    useEffect(() => {
+        console.log('useEffect callback executed')
+        fetch('https://kekambas-bs.herokuapp.com/kekambas')
+            .then(res => res.json())
+            .then(data =>{
+                let classmateData = data
+                setClassmates(classmateData)
+            })
+    }, []) 
+
+    function handleButtonClick(event){
+        event.preventDefault()
+        let newClassmates = event.target.button
+        setClassmates(newClassmates)
+        console.log('clicked')
+    }
+
   return (
-    <h1>Hello World</h1>
+    <div className='row justify-content-center'>
+        <button onClick={handleButtonClick} type="button" className="btn mt-4 text-white" name="button" style= {buttonStyle}>Next Classmate</button>
+        
+        {classmates.map((mates, idx) => {
+            return(
+                <div className="card mt-3" key={idx} style={cardStyle}>
+                    <img src="https://picsum.photos/200" className="card-img-top p-2" alt="..."/>
+                    <div className="card-body">
+                    <h3 className="card-text text-center">{mates.first_name}</h3>
+            </div>
+          
+        </div>
+            )
+        })}
+        
+    </div>
   )
 }
