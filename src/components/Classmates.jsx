90381f66af444ca4e02dbaada0c06ec4283f4d8d
Grace Alfonsi <cunniff.grace@gmail.com>
2022-08-30 23:33:46 -0500

    changed cards. Added Header. Gave up on button.

--- src/components/Classmates.jsx
+++ src/components/Classmates.jsx
@@ -3,12 +3,23 @@ import React, { useEffect, useState } from 'react'
 
 export default function Classmates() {
     const cardStyle = {
-        width: '18rem'
+        width: '18rem', 
+        backgroundColor: '#fb8b24'
     }
 
-    const buttonStyle = {
-        backgroundColor: '#6a4c93'
+    let headerStyle = {
+        fontFamily: 'Sacramento, cursive'
     }
+
+    // tried to make a function that would loop through different background colors for cards. Failed. : ( 
+    // let cardColors = ['#5f0f40', '#9a031e', '#fb8b24']
+    
+    // let colors = cardColors.map(color => color);
+
+
+    // const buttonStyle = {
+    //     backgroundColor: '#6a4c93'
+    // }
     
     // setClassmates is a function that takes in classmates data
     const [classmates, setClassmates] = useState([])
@@ -23,29 +34,34 @@ export default function Classmates() {
             })
     }, []) 
 
-    function handleButtonClick(event){
-        event.preventDefault()
-        let newClassmates = event.target.button
-        setClassmates(newClassmates)
-        console.log('clicked')
-    }
+    // function handleButtonClick(event){
+    //     event.preventDefault()
+    //     let newClassmates = event.target.button
+    //     setClassmates(newClassmates)
+    //     console.log('clicked')
+    // }
 
-  return (
-    <div className='row justify-content-center'>
-        <button onClick={handleButtonClick} type="button" className="btn mt-4 text-white" name="button" style= {buttonStyle}>Next Classmate</button>
-        
-        {classmates.map((mates, idx) => {
-            return(
-                <div className="card mt-3" key={idx} style={cardStyle}>
-                    <img src="https://picsum.photos/200" className="card-img-top p-2" alt="..."/>
-                    <div className="card-body">
-                    <h3 className="card-text text-center">{mates.first_name}</h3>
-            </div>
-          
+    return (
+    <>
+        <div className='row text-center mt-3' style= {headerStyle}>
+                <h1 className='display-1'>Kekambas 96 Rules  </h1>
         </div>
-            )
-        })}
-        
-    </div>
-  )
+        <div className='row justify-content-center'>
+            {/* <button onClick={handleButtonClick} type="button" className="btn mt-4 text-white" name="button" style= {buttonStyle}>Next Classmate</button> */}
+            
+            {classmates.map((mates, idx) => {
+                return(
+                    <div className="card m-3" key={idx} style={cardStyle}>
+                        {/* <img src="https://picsum.photos/200" className="card-img-top p-2" alt="..."/> */}
+                        <div className="card-body">
+                        <h3 className="card-text text-center">{mates.first_name} {mates.last_name}</h3>
+                </div>
+            
+            </div>
+                )
+            })}
+            
+        </div>
+    </>
+    )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Navbar() {
     const bgColor = {
-        backgroundColor: '#03045e'
+        backgroundColor: '#5f0f40'
     }
     
   return (
